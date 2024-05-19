eedb02585deb5517bf9b1788f737158558650ea3
Daniil <portal.sid.grand2@gmail.com>
2024-05-19 10:10:28 +0300

    update.

--- src/features/auth/ui/protected-pricing-page.tsx
+++ src/features/auth/ui/protected-pricing-page.tsx
@@ -9,13 +9,13 @@ export function protectedPricingPage<P>(Component: (props: P) => ReactElement) {
     const router = useRouter();
     const { isLoading, isError } = useSessionQuery();
 
-    if (isLoading) {
-      return <UiPageSpinner />;
-    }
+    // if (isLoading) {
+    //   return <UiPageSpinner />;
+    // }
 
-    if (isError) {
-      router.replace(ROUTES.SIGN_IN);
-    }
+    // if (isError) {
+    //   router.replace(ROUTES.SIGN_IN);
+    // }
 
     return <Component {...props} />;
   };
