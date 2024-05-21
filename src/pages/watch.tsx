5c168a428c2669f2f105dfded9b1b5fd856ace9d
Daniil <portal.sid.grand2@gmail.com>
2024-05-21 19:05:13 +0300

    fix

--- src/pages/watch.tsx
+++ src/pages/watch.tsx
@@ -3,7 +3,7 @@ import { UiWatchPageLayout } from "@/shared/ui/Layouts/UiWatchPageLayout";
 export function Watch() {
   return (
     <>
-      <UiWatchPageLayout closeSidebar={true}>
+      <UiWatchPageLayout closeSidebar={false}>
         <span className="text-black mt-[300px]">Main</span>
       </UiWatchPageLayout>
     </>
